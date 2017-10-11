a47f9cdac97fac84bc84f817b1016283c7f7d0c2
Drol825 <drol825@gmail.com>
2017-10-11 22:48:53 +0300

    change footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 ﻿ 	<div id="templatemo_footer">
     
-		Copyright © 2048 <a href="#">Your Company Name</a> | 
-        <a href="http://www.iwebsitetemplate.com" target="_parent">Website Templates</a> by <a href="http://www.templatemo.com" target="_parent">Free CSS Templates</a>
+		Copyright © 2017 <a href="#">Your Company Name</a> | 
+        
         
     </div>
   
