f9a43d5132d9d145bc323e657132ffb79b9f96c7
Drol825 <drol825@gmail.com>
2017-10-11 19:15:46 +0300

    comit 2

---
+++ footer.php
@@ -0,0 +1,12 @@
+﻿ 	<div id="templatemo_footer">
+    
+		Copyright © 2048 <a href="#">Your Company Name</a> | 
+        <a href="http://www.iwebsitetemplate.com" target="_parent">Website Templates</a> by <a href="http://www.templatemo.com" target="_parent">Free CSS Templates</a>
+        
+    </div>
+  
+    <div class="cleaner"></div>
+</div> <!-- end of warpper -->
+
+</body>
+</html>
