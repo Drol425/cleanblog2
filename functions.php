440d4569b8ce83dabb979cb9c7834f3d3b3cb9a5
Drol425 <drol825@gmail.com>
2017-10-14 13:03:53 +0400

    create function

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+register_sidebar(array(
+   'name' => __("New Widget Area", CURRENT_THEME),
+   'id' => 'new-widget-area-sidebar',
+   'description' => __("New Widget Area", CURRENT_THEME),
+   'before_widget' => '<div id="%1$s">',
+   'after_widget' => '</div>',
+   'before_title' => '<h4>',
+   'after_title' => '</h4>',
+));
+add_theme_support( 'post-thumbnails' ); // для всех типов постов
