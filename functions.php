31255a635a864a4da6ced5358218d040af497436
Drol425 <drol825@gmail.com>
2017-10-15 23:43:59 +0400

    funct and robots

--- functions.php
+++ functions.php
@@ -9,3 +9,33 @@ register_sidebar(array(
    'after_title' => '</h4>',
 ));
 add_theme_support( 'post-thumbnails' ); // для всех постов
+//Удаляем meta name generator (WP version)
+add_filter('the_generator', '__return_empty_string', 99);
+remove_action('wp_head', 'wp_generator', 99);
+
+function zacompom_remove_version() {//удаляем версию вордпресс
+return '';
+}
+add_filter('the_generator', 'zacompom_remove_version');
+function zacompom_wordpress_errors(){//убираем ошибку ввода данных в админке вордпресс
+  return '';
+}
+add_filter( 'login_errors', 'zacompom_wordpress_errors' );
+
+
+remove_filter( 'authenticate', 'wp_authenticate_email_password', 20 );//удаляем аунтефикацию с использованием почты
+
+// Включение коротких кодов в текстовых виджетах
+add_filter('widget_text','do_shortcode');
+
+function zacompom_imagelink_setup() {//удаляем ссылки изображений в постах
+	$image_set = get_option( 'image_default_link_type' );
+	
+	if ($image_set !== 'none') {
+		update_option('image_default_link_type', 'none');
+	}
+}
+add_action('admin_init', 'zacompom_imagelink_setup', 10);
+
+
+add_filter('xmlrpc_enabled', '__return_false');//удаляем xmlrpc
